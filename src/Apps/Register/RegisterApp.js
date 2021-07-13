4db1ffd4fbd9ef28fda9e6b441551732320651f8
viseth89 <viseth89@gmail.com>
2021-07-13 18:59:48 -0500

    added Export to register app

--- src/Apps/Register/RegisterApp.js
+++ src/Apps/Register/RegisterApp.js
@@ -1,1 +1,14 @@
 import React, {useState, useEffect} from "react";
+
+const RegisterApp = () => {
+
+
+    // Return -> Add html Code to be returned here
+    return (
+        <div className="main">
+            <h4>This is a test of the registerApp</h4>
+        </div>
+    )
+}
+
+export default RegisterApp
