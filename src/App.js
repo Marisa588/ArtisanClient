bc1a198249d887feae1adb68af675686a8db7268
viseth89 <viseth89@gmail.com>
2021-07-13 19:14:49 -0500

    Added export and import for Register, testing implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Register from './Components/Register/RegisterApp'
+import RegisterApp from './Components/Register/RegisterApp';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <RegisterApp />
       </header>
     </div>
   );
