788f7dc670421746258fcdeb39599d2afa1e8f05
Lauren Durr <laurenashdurr@gmail.com>
2021-07-13 21:47:44 -0400

    added scaffolding for the homepage and space for signup and login forms

---
+++ src/Components/Homepage.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Login from './Login';
+import mainImage from '../../src/radrecordshomepage.png'
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: `url(${mainImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+function Homepage() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7}  className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}></Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <Login/>
+          
+        </div>
+
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}></Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+
+          <Login/>
+
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Homepage;
---
+++ src/Components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FormGroup, Form, FormLabel, Input, Button } from '@material-ui/core'
+
+function Login() {
+    return (
+        <div>
+            <form>
+                <Input></Input>
+            </form>
+        </div>
+    )
+}
+
+export default Login; 
