6986fdd851be1d081ab650c057a4f8f8f19f2ca2
viseth89 <viseth89@gmail.com>
2021-07-20 12:54:16 -0500

    Added RecordIndex.js

---
+++ src/Components/Homepage/RecordIndex.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import {Container, Row, Col} from 'reactstrap'
+
+const RecordIndex = (props) => {
+    const [records, setRecords] = useState([]);
+
+    const fetchRecords = () => {
+        fetch('http://localhost:3000/log', {
+            method: "GET",
+            headers: new Headers({
+                'Content-Type':'application/json',
+                'Authorization': `Bearer ${props.token}`
+            })
+        }).then((res) => res.json())
+            .then((logData) => {
+                setWorkouts(logData);
+            })
+    };
+
+    useEffect(() => {
+        fetchWorkouts();
+    }, []);
+
+    return(
+        <Container>
+            <Row>
+                <col md='3'>
+                    {/* The create component will go here */}
+                </col>
+                <col md='9'>
+                    <h2>Log a record to see a table, this wil lbe added in later pages.</h2>
+                </col>
+            </Row>
+        </Container>
+    )
+};
+
+export default RecordIndex;
