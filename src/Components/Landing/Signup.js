5caf47b999233aabb87bd2a0534b0124e8f737b9
viseth89 <viseth89@gmail.com>
2021-07-21 07:18:37 -0500

    Setting validation for username

--- src/Components/Landing/Signup.js
+++ src/Components/Landing/Signup.js
@@ -1,48 +1,108 @@
-import React, {useState} from 'react';
-import { FormGroup, Form, FormLabel, Input, Button } from '@material-ui/core'
+import React, { useState } from "react";
+import { FormGroup, Form, FormLabel, Input, Button } from "@material-ui/core";
 
 const Signup = (props) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    
-    // Validation
-    const [cPassword, setCPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [emailValid, setEmailValid] = useState(false);
-    const [passwordValid, setPasswordValid] = useState(false)
-    const [cPasswordValid, setCPasswordValid] = useState(false);
+  // Validation
+  const [cPassword, setCPassword] = useState("");
 
-    
-    let handleSubmit = (event) => {
-        event.preventDefault();
-        fetch("http://localhost:3001/user/register", {
-            method: "POST",
-            body: JSON.stringify({user:{username: username, password: password}}),
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            })
-        }).then(
-            (response) => response.json()
-        ).then((data) => {
-            props.updateToken(data.sessionToken)
-        })
-    }
+  const [userValid, setUserValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [cPasswordValid, setCPasswordValid] = useState(false);
 
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <FormGroup>
-                    <FormLabel htmlFor='username'>Username</FormLabel>
-                    <Input onChange={(e) => setUsername(e.target.value)} name="username" value={username}/>
-                </FormGroup>
-                <FormGroup>
-                    <FormLabel htmlFor="password">Password</FormLabel>
-                    <Input onChange={(e) => setPassword(e.target.value)} name="password" value={password}/>
-                </FormGroup>
-                <Button type='submit'>Sign Up</Button>
-            </form>
-        </div>
-    )
-}
+  // Begin Css
+  const formWrapper = {
+    marginInline: "1em",
+    width: "24em",
+    minWidth: "12em",
+    background: theme === "Dark" ? "rgba(0,0,0,.5)" : "rgba(255,255,255, .6)",
+    borderRadius: "1em",
+    padding: "2em",
+    backdropFilter: "blur(15px)",
+    filter: "drop-shadow(5px 5px 5px rgba(0,0,0,.3))",
+    color: theme === "Dark" ? "white" : "black",
+  };
+
+  const formFeedBackStyle = {
+    fontSize: "1.2em",
+    fontWeight: "bold",
+    userSelect: "none",
+  };
+
+  const inputStyle = {
+    textAlign: "center",
+    color: theme === "Dark" ? "white" : "black",
+  };
+
+  // End Css
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3001/user/register", {
+      method: "POST",
+      body: JSON.stringify({
+        user: { username: username, password: password },
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        props.updateToken(data.sessionToken);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <FormGroup>
+          <FormLabel htmlFor="username">Username</FormLabel>
+          <Input
+            required
+            type='username'
+            id='username'
+            onChange={(e) => {
+                setUsername(e.target.value)
+                if (
+                    e.target.value.includes("@")
+                ) {
+                    setUserNameValid(true);
+                } else {
+                    setUserNameValid(false);
+                }
+
+
+            }}
+            name="username"
+            value={username}
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            onChange={(e) => {
+                setPassword(e.target.value)
+                if (
+                    e.target.value.includes("@")
+                ) {
+                    setPasswordValid(true);
+                } else {
+                    setPasswordValid(false);
+                }
+            
+            
+            }}
+            name="password"
+            value={password}
+          />
+        </FormGroup>
+        <Button type="submit">Sign Up</Button>
+      </form>
+    </div>
+  );
+};
 
 export default Signup;
